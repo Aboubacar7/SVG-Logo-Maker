c7ca84649d10b096dae286d97461323af2bb593f
Aboubacar7 <abdrago17@yahoo.com>
2023-03-11 14:19:14 -0800

    adding test

---
+++ tests/Triangle.test.js
@@ -0,0 +1,19 @@
+const { default: test } = require('node:test');
+const { describe } = require('yargs');
+const Triangle =require('../lib/Triangle');
+
+describe('Triangle', () => {
+    test('Should render a triangle shape', () => {
+      const shape = new Triangle();
+      shape.setColor("blue");
+      expect(shape.render()).toEqual('<polygon class="shape" x="10" y="10" points="150,30 244,182 56,182" style="fill:blue; stroke:#000" fill="blue"/>');
+    })
+    test('Should render text in the center of the triangle', () => {
+        const shape = new Triangle('Hello World', 'white', 'blue');
+        const expectedOutput = '<text class="text" x="150" y="131.33" font-size="40" text-anchor="middle" style="fill:white">Hello World</text>';
+        expect(shape.renderText()).toEqual(expectedOutput);
+      });
+      
+  })
+  
+  
